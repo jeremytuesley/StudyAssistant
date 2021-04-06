d58676970ae3008ba4b1171f7604156ef677c667
jeremytuesley <jeremytuesley@gmail.com>
2021-04-06 14:39:37 +1000

    basemenu completed, empty files included

---
+++ src/main.rb
@@ -0,0 +1,23 @@
+quit = false
+menu = true
+until quit
+  if menu
+    puts "What would you like to do? options: 'play' 'instructions' 'create more questions' 'manage questions' 'quit'"
+    menu = false
+  end
+  case (gets.chomp.downcase)
+  when 'play'
+    puts 'do this play'
+  when 'create more questions'
+    puts 'do this create'
+  when 'manage questions'
+    puts 'do this manage'
+  when 'back to menu'
+    menu = true
+  when 'quit'
+    puts 'Thanks! Goodbye :)'
+    quit = true
+  else
+    puts 'Input not recognized, please try again'
+  end
+end
