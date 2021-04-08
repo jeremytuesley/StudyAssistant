38a0827c061706601e39a96ddd0b6cf5627916f6
jeremytuesley <jeremytuesley@gmail.com>
2021-04-08 20:35:14 +1000

    instructions added, some mmenu alterations, welcome message added

--- src/main.rb
+++ src/main.rb
@@ -1,15 +1,19 @@
 require_relative './MenuOptions/play'
 require_relative './MenuOptions/create'
+require_relative './MenuOptions/instructions'
 quit = false
-
+puts ''
 puts 'Welcome! My name is Study Assitant.'
-puts "At any time, type 'back' to go back"
+puts "At any time within the menu options, type 'back' to go back up the previous menu"
+puts ''
 
 until quit
   puts "What would you like to do? options: 'play' 'instructions' 'create more questions' 'manage questions' 'quit'"
   case (gets.chomp.downcase)
   when 'play'
     puts 'do this play'
+  when 'instructions'
+    instructions
   when 'create more questions'
     createContent
   when 'manage questions'
@@ -17,6 +21,8 @@ until quit
   when 'quit'
     puts 'Thanks for playing! Goodbye :)'
     quit = true
+  when 'back'
+    puts "You're already in the menu! If you wanted to quit, type 'quit'"
   else
     puts 'Input not recognized, please try again'
   end
