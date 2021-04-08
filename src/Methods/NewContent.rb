4d1671b00d3adfc384bdcfb44f317fd6a84402c2
jeremytuesley <jeremytuesley@gmail.com>
2021-04-08 16:15:39 +1000

    question function loop created, needs optimisation

--- src/Methods/NewContent.rb
+++ src/Methods/NewContent.rb
@@ -27,41 +27,58 @@ def createCategory # Function to create a category if chosen from the menu
 end
 
 def createQuestion
-  categoryFile = File.read(FilePath)
-  data_array = JSON.parse(categoryFile)
+  back = false
+  until back
+    categoryFile = File.read(FilePath)
+    data_array = JSON.parse(categoryFile)
 
-  # Checking if category exists so question can be assigned to it
-  puts 'Which category does your question fall under?'
-  data_array.each { |item| print "#{item['category']}, " }
-  puts ''
-  cata_input = gets.chomp
-  data_array.each do |item|
-    if cata_input != 'back'
-      if item['category'].downcase == cata_input.downcase
-        # Creating the question and checking if it exists already also
-        puts 'Please enter your question:'
-        input = gets.chomp
-        question_taken = false
-        item['content'].each do |content|
-          if content['question'].downcase == input.downcase
-            puts 'That question already exists'
-            question_taken = true
+    # Checking if category exists so question can be assigned to it
+    puts 'Which category does your question fall under?'
+    data_array.each { |item| print "#{item['category']}, " }
+    puts ''
+    cata_input = gets.chomp
+    data_array.each do |item|
+      puts 'data_array start'
+      puts cata_input
+      back = true if cata_input == 'back'
+      if !back
+        if item['category'].downcase == cata_input.downcase
+          # Creating the question and checking if it exists already also
+          puts 'Please enter your question:'
+          input = gets.chomp
+          question_taken = false
+          back = true if input == 'back'
+          if input != 'back'
+            item['content'].each do |content|
+              if content['question'].downcase == input.downcase
+                puts 'That question already exists'
+                question_taken = true
+                break
+              end
+            end
+            if question_taken == false
+              puts 'Please enter the answer:'
+              ans_input = gets.chomp.downcase
+              item['content'] << { question: input, answer: ans_input }
+              File.open(FilePath, 'w') { |f| f.write(data_array.to_json) }
+              puts 'Successfully added new question'
+              break
+            end
+          end
+        elsif item['category'].downcase != cata_input.downcase
+          puts "That category doesn't exist, try again? y/n"
+          input = gets.chomp
+          if input == 'y'
+            createQuestion
+          else
             break
           end
-        end
-        if question_taken == false
-          puts 'Please enter the answer:'
-          ans_input = gets.chomp.downcase
-          item['content'] << { question: input, answer: ans_input }
-          File.open(FilePath, 'w') { |f| f.write(data_array.to_json) }
-          puts 'Successfully added new question'
-        else
-          createQuestion
         end
       end
     end
   end
 end
+
 puts 'reached here1'
 createQuestion
 puts 'reached here2'
