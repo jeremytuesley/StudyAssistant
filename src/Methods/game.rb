c9be96d3a33f8cb579176ca7502e1e8b7f2a26a2
jeremytuesley <jeremytuesley@gmail.com>
2021-04-10 11:10:49 +1000

    Adding documentation to game file

--- src/Methods/game.rb
+++ src/Methods/game.rb
@@ -3,18 +3,19 @@ require 'colorize'
 require_relative '../Methods/utils'
 
 def categoryPlay 
-
+ #Choosing a category which program will pull questions from
   puts 'Please choose a category'.cyan
   choice = displayCategory
   if choice != 'Back to Menu'
     category = findCategory(choice)
+    # Start of game
     result = sampleQuestion(category["content"])
-    report(result) if result 
+    report(result) if result #Correct or incorrect feedback
   end
 end
 
 def randomPlay 
-  # Sample a random category
+  # Chooses to let the program to give a random category to user, pulls questions from that category
   dataArray = JSON.parse(File.read(FilePath))
   category = dataArray.sample
 
@@ -27,15 +28,17 @@ def randomPlay
 
   # Start game
   result = sampleQuestion(category["content"])
-  report(result) if result 
+  report(result) if result #Correct or incorrect feedback
 end
 
 def mayhemPlay 
+  # Ignores categories and gives out all the questions
   dataArray = JSON.parse(File.read(FilePath))
   allQuestinos = []
   dataArray.each do |item|
     allQuestinos.concat item['content']
   end
+  #Start of Game
   result = sampleQuestion(allQuestinos)
-  report(result) if result 
+  report(result) if result #Correct or incorrect feedback
 end
